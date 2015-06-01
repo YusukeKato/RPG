c8a97e7fa60c82cbd104f94981ca97ecf3f71053
Shimada <t62b85lin7xudc@i.softbank.jp>
2015-06-02 06:23:01 +0900

    ???

---
+++ file.c
@@ -0,0 +1,43 @@
+
+#include <stdio.h>
+#include "file.h"
+
+int player_write(struct player p)
+{
+	FILE *fp;
+	
+	fp = fopen(filename, "w");
+	if(!fp) {
+		fprintf(stderr, "Error: file open\n");
+		return -1;
+	}
+	
+	fprintf(fp, "%d\n%s\n%d\n",
+		p.level,
+		p.name,
+		p.job);
+	
+	fclose(fp);
+	
+	return 0;
+}
+
+int player_read(struct player *p)
+{
+	FILE *fp;
+	
+	fp = fopen(filename, "r");
+	if(!fp) {
+		fprintf(stderr, "Error: file open\n");
+		return -1;
+	}
+	
+	fscanf(fp, "%d\n%d\n%d\n",
+		&(p->level),
+		&(p->name),
+		&(p->job));
+	
+	fclose(fp);
+	
+	return 0;
+}
