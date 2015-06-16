2bf65730faa50dc14f6bed3452857ef4d71d63f4
YusukeKato <mikan1212.lion2a4p8c3@docomo.ne.jp>
2015-06-16 15:29:29 +0900

    20150616Kato

---
+++ game.c
@@ -0,0 +1,38 @@
+/*
+ * Date   : 20150602
+ * Name   : Yusha
+ *
+ * Writer:  Yusuke Kato
+ */
+
+/*
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<math.h>
+#include<windows.h>
+#include "file.h"
+ 
+int Yuusha();
+
+int Yuusha()
+{
+	int c;
+	
+	printf("\n半年前に復活した魔王によって、世界は滅びようとしていた。\n"
+		   "そこで各国は、魔王討伐のために軍隊を向かわしたが、\n"
+		   "魔王軍の前では無力であった。\n"
+		   "何とかしなくてはと考えた国王は”伝説の勇者”を探し出し、\n"
+		   "その者に魔王討伐を託すことにした。\n\n\n\n");
+		   
+	printf("%s は目覚めた。窓から入ってくる太陽の光がまぶしい。\n", p.name);
+	
+	player_write(p);
+	
+	printf("Please any key.\n");
+	c = fgetc(stdin);	   
+	
+	return;
+}
+
+*/
